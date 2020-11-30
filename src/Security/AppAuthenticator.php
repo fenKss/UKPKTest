9058511aea833eff8552cfade1a644a73ded81e6
Сюрсин Михаил <mixail.syursin@gmail.com>
2020-11-30 11:31:23 +0600

    add: Fixtures

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -97,7 +97,8 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('default'));
+//        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
