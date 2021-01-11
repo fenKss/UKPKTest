acce9af9754c2c07662dccc3b31f05df1ec7dd68
Mixail Syursin <mixail.syursin@gmail.com>
2021-01-11 11:38:27 +0600

    Обновление статуса в лк тестов если они уже начаты/закончены

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\ENum\EUserTestStatus;
 use App\Repository\OlympRepository;
+use Carbon\Carbon;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,7 +25,30 @@ class UserController extends AbstractController
      */
     public function index(OlympRepository $olympRepository): Response
     {
+        /**
+         * @var ?User $user
+         */
         $user = $this->getUser();
+        if (!$user){
+            return $this->redirectToRoute('default');
+        }
+        $em = $this->getDoctrine()->getManager();
+        foreach ($user->getUserTests() as $userTest){
+            $tour = $userTest->getVariant()->getTest()->getTour();
+            $now = new Carbon();
+            if ($now > $tour->getStartedAt() && $now < $tour->getExpiredAt()){
+                if ($userTest->getStatus() != EUserTestStatus::STARTED_TYPE){
+                    $userTest->setStatus(EUserTestStatus::STARTED_TYPE);
+                    $em->persist($userTest);
+                }
+            }elseif($now >= $tour->getExpiredAt()){
+                if ($userTest->getStatus() != EUserTestStatus::FINISHED_TYPE) {
+                    $userTest->setStatus(EUserTestStatus::FINISHED_TYPE);
+                    $em->persist($userTest);
+                }
+            }
+        }
+        $em->flush();
         $olymps = $olympRepository->getByUser($user);
 
         return $this->render('user/index.html.twig', [
